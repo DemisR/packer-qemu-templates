e9a253e17882ba344c015a4c5b47de6016aac1ad
Demis Rizzotto <demis.rizzotto@exxoss.com>
2018-12-17 15:24:08 +0100

    clean

--- ubuntu/scripts/packages.sh
+++ ubuntu/scripts/packages.sh
@@ -1,17 +1,7 @@
-DEV_PACKAGES="
+PACKAGES="
+ntp
 curl
 htop
 cloud-init
 "
-
-ESSENTIAL_PACKAGES="
-ntp
-nfs-common
-"
-
-if [[ $INSTALL_DEV_PACKAGES  =~ true || $INSTALL_DEV_PACKAGES =~ 1 ||
-        $INSTALL_DEV_PACKAGES =~ yes ]]; then
-  apt-get -y install $DEV_PACKAGES
-fi
-
-apt-get -y install $ESSENTIAL_PACKAGES
+apt-get -y install $PACKAGES
